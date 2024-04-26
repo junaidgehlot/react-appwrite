64d9a84d663bf58aada6c625462a6964be3181f5
Junaid Gehlot <junaid@pictory.ai>
2024-04-27 00:02:56 +0530

    [feat] updated routes

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,7 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo/Logo";
-export { Header, Footer, Container, Logo, LogoutBtn }
+import PostCard from "./PostCard/PostCard";
+import Login from "./Login/Login";
+import Input from "./Input/Input";
+export { Header, Footer, Container, Logo, LogoutBtn, PostCard, Login, Input }
